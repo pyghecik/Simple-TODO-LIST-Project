6fdf820e8564f9177fb173d93f655718cf7c48ac
Paweł <127633002+pyghecik@users.noreply.github.com>
2023-11-01 18:45:54 +0100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,9 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  output: {
-    export: true,
-  },
+  output: "export",
 };
 
 module.exports = nextConfig;
